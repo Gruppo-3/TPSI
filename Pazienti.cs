5ef757e7195a350601e81104181874c940d513ab
IL-FRANZO <64190974+IL-FRANZO@users.noreply.github.com>
2020-06-14 16:36:57 +0200

    Add files via upload

---
+++ Pazienti.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Libreria.Gruppo3
+{
+    public class Pazienti
+    {
+            public string Nome
+        { get; set; }
+        public string Cognome
+        { get; set; }
+        public string CF
+        { get; set; }
+        public string CodicePriorita
+        { get; set; }
+        public DateTime Data
+        { get; set; }
+        public Pazienti()
+        {
+
+        }
+
+        public Pazienti(string nome, string congome, string cf, string codice)
+        {
+             Nome = nome;
+            Cognome = congome;
+            CF = cf;
+            CodicePriorita = codice;
+            Data = DateTime.Now;
+        }
+    }
+}
